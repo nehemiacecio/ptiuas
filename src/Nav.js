53f01279c376e46a00da102037ab52d174e098a5
nehemiacecio <nehemia.cecio.3@gmail.com>
2020-12-19 03:06:16 +0700

    sudah

--- src/Nav.js
+++ src/Nav.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react'
 import {Link} from "react-router-dom";
 
 export class Nav extends Component {
+    state={
+        toggle: false
+         
+    }
+    menuToggle = ()=>{
+        this.setState({toggle: !this.state.toggle})
+    }
+        
     render() {
+        const {toggle} =this.state;
         return (
             <div>
-                <ul>
+                <ul className={toggle? "toggle" : ""}>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/">Home</Link></li>
+                    <li className="close" onClick={this.menuToggle}>X</li>
                     </ul>
+                    <div className="menu" onClick={this.menuToggle}>Menu</div>
             </div>
         )
     }
